55775dbe3e2f894908151a56a8955e5ade470810
HannyaMask07 <krzysztof.trytek07@gmail.com>
2023-04-27 21:02:17 +0200

    push test
    Hello pitek by HannyaMask07

--- API-project/WebApi/Program.cs
+++ API-project/WebApi/Program.cs
@@ -6,7 +6,7 @@ builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
-
+//Hello pitek
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
