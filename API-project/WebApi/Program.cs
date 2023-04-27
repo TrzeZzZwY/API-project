81421a3e4b9eb2eff7748f367a4dd62e6d488d9d
Jan Świder <jasiek.swider@outlook.com>
2023-04-27 21:44:52 +0200

    Added some projects to solution
    added AppCore, Infrastructure, UnitTest

--- API-project/WebApi/Program.cs
+++ API-project/WebApi/Program.cs
@@ -1,15 +1,11 @@
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
-// Hello world 2
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
-//Hello pitek
 var app = builder.Build();
-//Testujemy
-//Hello world 3
 
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
@@ -17,7 +13,6 @@ if (app.Environment.IsDevelopment())
     app.UseSwagger();
     app.UseSwaggerUI();
 }
-//pitek to przyjaciel
 app.UseHttpsRedirection();
 
 app.UseAuthorization();
