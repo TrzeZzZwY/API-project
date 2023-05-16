c0a2b405db96b52b1aebe0ab63a0e809ee1b9984
Jan Świder <jasiek.swider@outlook.com>
2023-05-16 20:17:18 +0200

    IAlbumService

--- API-project/AppCore/Interfaces/Services/IAlbumService.cs
+++ API-project/AppCore/Interfaces/Services/IAlbumService.cs
@@ -9,6 +9,24 @@ namespace AppCore.Interfaces.Services
 {
     public interface IAlbumService
     {
-        //IEnumerable<PublishAlbum> 
+        public IEnumerable<PublishAlbum> GetAll();
+        public IEnumerable<PublishAlbum> GetAllFor(Guid userId);
+        public IEnumerable<PublishAlbum> GetAllFor(string userLogin);
+
+        public PublishAlbum GetByOne(Guid publishAlbumId);
+        public PublishAlbum GetByOne(Guid userID,string albumName);
+        public PublishAlbum GetByOne(string userLogin,string albumName);
+
+        public PublishAlbum Create(Guid userId,PublishAlbum album);
+
+        public PublishAlbum Delete(Guid UserId,PublishAlbum album);
+        public PublishAlbum Delete(string userLogin, PublishAlbum album);
+        public PublishAlbum Delete(Guid userId, string albumName);
+        public PublishAlbum Delete(string userLogin, string albumName);
+        public PublishAlbum Delete(Guid albumId);
+
+
+        public PublishAlbum Update(Guid userId, PublishAlbum album);
+        public PublishAlbum Update(string userLogin, PublishAlbum album);
     }
 }
