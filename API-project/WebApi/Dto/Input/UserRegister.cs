08f03a701c68281f6f8044a326957b0390d9b6e1
Jan Świder <jasiek.swider@outlook.com>
2023-06-12 17:41:52 +0200

    fix

--- API-project/WebApi/Dto/Input/UserRegister.cs
+++ API-project/WebApi/Dto/Input/UserRegister.cs
@@ -13,7 +13,7 @@ namespace WebApi.Dto.Input
         [RegularExpression(@"^[a-zA-Z0-9_]+$", ErrorMessage = "User login can only contain letters, digits, and underscores.")]
         public string Login { get; set; }
         public string Password { get; set; }
-        [Required(ErrorMessage = "Email is required"]
+        [Required(ErrorMessage = "Email is required")]
         [StringLength(16, ErrorMessage = "Email must be between 5 and 50 characters", MinimumLength = 5)]
         [RegularExpression("^[a-zA-Z0-9_.-]+@[a-zA-Z0-9-]+.[a-zA-Z0-9-.]+$", ErrorMessage = "Email must be a valid email")]
         public string Email { get; set; }
